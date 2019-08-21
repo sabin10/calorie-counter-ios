2b26afaedec326de30eaeaae759a6683a7263a1f
sabinhantu <sabinhantu@gmail.com>
2019-08-21 17:13:06 +0300

    add firebase + auth storyboard

---
+++ calorie-counter/LoginController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginController.swift
+//  calorie-counter
+//
+//  Created by Sabin on 21/08/2019.
+//  Copyright © 2019 Sabin. All rights reserved.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
