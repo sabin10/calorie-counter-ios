62a0a62acc5d934695bd6788f916782a32f6a949
sabinhantu <sabinhantu@gmail.com>
2019-08-21 15:30:37 +0300

    Project Initialization

---
+++ calorie-counter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calorie-counter
+//
+//  Created by Sabin on 21/08/2019.
+//  Copyright © 2019 Sabin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
